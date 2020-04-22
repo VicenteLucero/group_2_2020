491ab3d356851b825b95e6ed0f64f34888897d99
MEspinosaGH <mespinosa3@miuandes.cl>
2020-04-22 12:06:28 -0400

    functions to retrieve arguments from commandline

--- load_block_model.py
+++ load_block_model.py
@@ -2,37 +2,131 @@ import os
 import csv
 import sys, getopt
 
-def mainLoad(argv):
-   inputfile = ''
-   outputfile = ''
-   columnsfile = ''
-   try:
-      opts, args = getopt.getopt(argv,"i:c:o:",["ifile=", "cfile=", "ofile="])
-   except getopt.GetoptError:
-      print('main.py -L -i <inputfile> -c <columnsFile> -o <outputfile>')
-      sys.exit(2)
-   for opt, arg in opts:
-      if opt in ("-i", "--ifile"):
-         inputfile = arg
-      elif opt in ("-c", "--cfile"):
-         columnsfile = arg
-      elif opt in ("-o", "--ofile"):
-         outputfile = arg
+def loadModelArguments(argv):
+    inputfile = ''
+    outputfile = ''
+    columnsfile = ''
+    try:
+        opts, args = getopt.getopt(argv,"i:c:o:",["ifile=", "cfile=", "ofile="])
+    except getopt.GetoptError:
+        print('main.py -L -i <inputfile> -c <columnsFile> -o <outputfile>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ("-i", "--ifile"):
+            inputfile = arg
+        elif opt in ("-c", "--cfile"):
+            columnsfile = arg
+        elif opt in ("-o", "--ofile"):
+            outputfile = arg
 
-   LoadBlockModel(inputfile, columnsfile, outputfile)
+    LoadBlockModel(inputfile, columnsfile, outputfile)
 
-def mainPrint(argv):
-   inputfile = ''
-   try:
-      opts, args = getopt.getopt(argv,"i:",["ifile="])
-   except getopt.GetoptError:
-      print('main.py -P -i <inputfile>')
-      sys.exit(2)
-   for opt, arg in opts:
-      if opt in ("-i", "--ifile"):
-         inputfile = arg
+def printModelArguments(argv):
+    inputfile = ''
+    try:
+        opts, args = getopt.getopt(argv,"i:",["ifile="])
+    except getopt.GetoptError:
+        print('main.py -P -i <inputfile>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ("-i", "--ifile"):
+            inputfile = arg
          
-   PrintBlockModel(inputfile)
+    PrintBlockModel(inputfile)
+
+def numberOfBlocksArguments(argv):
+    block_model_name = ""
+
+    try:
+        opts, args = getopt.getopt(argv, "b:", ["bname="])
+    except getopt.GetoptError:
+        print('main.py -N -b <block_model_name>')
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt in ("-b", "--bname"):
+            block_model_name = arg
+
+    print(block_model_name)
+
+def massInKilogramsArgument(argv):
+    block_model_name = ""
+    x = 0
+    y = 0
+    z = 0
+
+    try:
+        opts, args = getopt.getopt(argv, "b:x:y:z:", ["bname=", "xcoord=", "ycoord=", "zcoord="])
+    except getopt.GetoptError:
+        print('main.py -M <block_model_name> <block_x> <block_y> <block_z>')
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt in ("-b", "--bname"):
+            block_model_name = arg
+        elif opt in ("-x", "--xcoord"):
+            x = int(arg)
+        elif opt in ("-y", "--ycoord"):
+            y = int(arg)
+        elif opt in ("-z", "--zcoord"):
+            z = int(arg)
+
+    print(block_model_name, x, y, z)
+
+def gradeInPercentageArguments(argv):
+    block_model_name = ""
+    x = 0
+    y = 0
+    z = 0
+    mineral_name = ""
+
+    try:
+        opts, args = getopt.getopt(argv, "b:x:y:z:m:", ["bname=", "xcoord=", "ycoord=", "zcoord=", "mname="])
+    except getopt.GetoptError:
+        print('main.py -G -b <block_model_name> -x <block_x> -y <block_y> -z <block_z> -m <mineral_name>')
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt in ("-b", "--bname"):
+            block_model_name = arg
+        elif opt in ("-x", "--xcoord"):
+            x = int(arg)
+        elif opt in ("-y", "--ycoord"):
+            y = int(arg)
+        elif opt in ("-z", "--zcoord"):
+            z = int(arg)
+        elif opt in ("-m", "--mname"):
+            mineral_name = arg
+
+    print(block_model_name, x, y, z, mineral_name)
+
+def attributeArguments(argv):
+    # main.py -A -b <block_model_name> -x <block_x> -y <block_y> -z <block_z> -n <attribute_name>
+    block_model_name = ""
+    x = 0
+    y = 0
+    z = 0
+    attribute_name = ""
+
+    try:
+        opts, args = getopt.getopt(argv, "b:x:y:z:n:", ["bname=", "xcoord=", "ycoord=", "zcoord=", "aname="])
+    except getopt.GetoptError:
+        print('main.py -A -b <block_model_name> -x <block_x> -y <block_y> -z <block_z> -n <attribute_name>')
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt in ("-b", "--bname"):
+            block_model_name = arg
+        elif opt in ("-x", "--xcoord"):
+            x = int(arg)
+        elif opt in ("-y", "--ycoord"):
+            y = int(arg)
+        elif opt in ("-z", "--zcoord"):
+            z = int(arg)
+        elif opt in ("-n", "--aname"):
+            attribute_name = arg
+
+    print(block_model_name, x, y, z, attribute_name)
 
 def LoadBlockModel(input_name, columns_name, output_name):
 
@@ -53,7 +147,10 @@ def LoadBlockModel(input_name, columns_name, output_name):
                 try:
                     csv_line[i] = int(csv_line[i])
                 except:
-                    csv_line[i] = float(csv_line[i])
+                    try:
+                        csv_line[i] = float(csv_line[i])
+                    except:
+                        csv_line[i] = str(csv_line[i])
             wr.writerow(csv_line)
 
 def PrintBlockModel(input_name):
--- main.py
+++ main.py
@@ -1,14 +1,37 @@
 import os
 import csv
 import sys, getopt
-from load_block_model import *
+from load_block_model import loadModelArguments, printModelArguments, numberOfBlocksArguments, massInKilogramsArgument, gradeInPercentageArguments, attributeArguments
 
 if __name__ == "__main__":
     if sys.argv[1] == '-L':
-        mainLoad(sys.argv[2:])
+        loadModelArguments(sys.argv[2:])
     elif sys.argv[1] == '-P':
-        mainPrint(sys.argv[2:])
+        printModelArguments(sys.argv[2:])
+    elif sys.argv[1] == '-N':
+        numberOfBlocksArguments(sys.argv[2:])
+    elif sys.argv[1] == '-M':
+        massInKilogramsArgument(sys.argv[2:])
+    elif sys.argv[1] == '-G':
+        gradeInPercentageArguments(sys.argv[2:])
+    elif sys.argv[1] == '-A':
+        attributeArguments(sys.argv[2:])
     else:
         print('\nAvailable commands:\n')
-        print('main.py -L -i <inputfile> -o <outputfile>')
+        print('main.py -L -i <inputfile> -c <columnsFile> -o <outputfile>')
+        print('Load file. Needs input file name and output file name.')
+        print('----')
         print('main.py -P -i <inputfile>')
+        print('Print file. Needs input file name.')
+        print('----')
+        print('main.py -N  -b <block_model_name>')
+        print('Number of Blocks of a stored block model. Needs block model name and number of blocks.')
+        print('----')
+        print('main.py -M -b <block_model_name> -x <block_x> -y <block_y> -z <block_z>')
+        print('Mass in Kilograms of one block in a stored block model. Needs block model name and coordinates.')
+        print('----')
+        print('main.py -G -b <block_model_name> -x <block_x> -y <block_y> -z <block_z> -m <mineral_name>')
+        print('Grade in Percentage for each Mineral of one block in a stored block model. Needs block model name, coordinates and mineral name.')
+        print('----')
+        print('main.py -A -b <block_model_name> -x <block_x> -y <block_y> -z <block_z> -n <attribute_name>')
+        print('Search attribute of one block in a stored block model. Needs block model name, coordinates and attribute name.')
