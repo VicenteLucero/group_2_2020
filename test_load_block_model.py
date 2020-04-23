60f8dfa4163ea4e0ae0f0e8b1c195ef02a9886a9
VicenteLucero <vjlucero@miuandes.cl>
2020-04-22 20:09:58 -0400

    added tests

---
+++ test_load_block_model.py
@@ -0,0 +1,11 @@
+import unittest
+from block import Block
+from block_model import BlockModel
+from load_block_model import *
+
+# class TestLoadBlockModel(unittest.TestCase):
+
+    # test para obtener el grade de algun mineral es el mismo que en test_block ya que llama a dicha funcion para un
+    # bloque en especifico
+    # La mayoria de las funciones llaman a funciones propias de block y block model por lo que sus respectivos test
+    # estan en sus respectivos archivos
